28547afd6d4210947d0b54e4373fe78ad11d997a
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-09-13 15:48:06 +0530

    Initial Commit

---
+++ dsa.cpp/recurssion1.cpp
@@ -0,0 +1,82 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int c=0;
+// void print()
+// {
+//     if(c==5)
+//     return ;
+//     else 
+//     cout<<"NAME"<<endl ;
+//     c++;
+//     print();
+// }
+// int main()
+// {
+//     print();
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int i=1,num;
+// void print( int a)
+// {
+//     if(i==num+1)
+//     return;
+//     else
+//     {
+//         cout<<" "<<i;
+//         i++;
+//         print(a);
+//     }
+// }
+// int main()
+// {   cout<<"Enter the value of N";
+//     cin >> num;
+//     print(num);
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print(int i, int n)
+// {
+//     if(n<i)
+//     return;
+//     cout<<" "<<n;
+//     print(i,n-1);
+//     }
+// int main ()
+// {
+//     cout<<"Enter a initial number i"<<endl<<" ENter a number N ";
+//     int ini, num;
+//     cin >> ini >> num;
+//     print(ini,num);
+
+// }
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void print(int i, int n)
+// {
+//     if(n<i)
+//     return;
+//     print(i,n-1);
+//     cout<<" "<<n;
+//     }
+// int main ()
+// {
+//     cout<<"Enter a initial number i"<<endl<<" ENter a number N ";
+//     int ini, num;
+//     cin >> ini >> num;
+//     print(ini,num);
+
+// }
+
+
---
+++ oddeven.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a;
+cout<<"Enter a Number"<<endl;
+cin>>a;
+if(a%2==0)
+{
+    cout<<a<<"\tis a Even Number";
+}
+else
+{cout<<a<<"\tis Odd Number";
+
+}
+
+    
+    
+    
+    return 0;}
