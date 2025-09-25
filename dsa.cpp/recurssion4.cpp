0aeaedef171e47fbe23fe65336ab56b3525023e7
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-09-25 12:10:32 +0530

    Fibbonacchi using Recurssion

--- dsa.cpp/recurssion4.cpp
+++ dsa.cpp/recurssion4.cpp
@@ -1,1 +1,18 @@
+//Fibbonacchi
 #include<bits/stdc++.h>
+using namespace std;
+int fibb(int n)
+{
+    if(n<=1)
+    return n;
+    else 
+    return fibb(n-1)+fibb(n-2);
+}
+int main()
+{   int num;
+   cout<<" Emter a number "<<endl;
+   cin>>num;
+   int fibbo =fibb(num);
+   cout<< fibbo; 
+}
+
